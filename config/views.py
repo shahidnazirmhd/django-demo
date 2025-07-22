c9debf3e75aa2523d48432dd5441383777c20c7e
Shahid Nazir <shahidnazirmhd@gmail.com>
2025-07-22 07:58:25 +0000

    Dynamic URL - DTL url tag

--- config/views.py
+++ config/views.py
@@ -1,10 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.urls import reverse
 
 def home(request):
     challenges_url = reverse("challenges-index")  # Named route from your app
-    response_data = f"""
-        <h1>Welcome to the Monthly Challenges Project!</h1>
-        <p>Click <a href="{challenges_url}">here</a> to navigate to the challenges.</p>
-    """
-    return HttpResponse(response_data)
+    return render(request, "config/index.html", {"render_url": challenges_url})
